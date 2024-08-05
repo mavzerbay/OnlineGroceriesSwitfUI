d99b1a8cc01d803702f77976e3b7ab6eed5a8fe5
Erbay MAVZER <mavzerbay@gmail.com>
2024-08-05 14:40:22 +0300

    Initial Commit

---
+++ OnlineGroceriesSwitfUI/OnlineGroceriesSwitfUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlineGroceriesSwitfUIApp.swift
+//  OnlineGroceriesSwitfUI
+//
+//  Created by Erbay MAVZER on 5.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct OnlineGroceriesSwitfUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
